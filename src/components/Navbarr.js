356c6ae9db6eb08b2e9101917ee8edea3b08fa84
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-03-24 10:55:47 +0530

    Updated portfolio

--- src/components/Navbarr.js
+++ src/components/Navbarr.js
@@ -1,29 +1,30 @@
 import {Container, Nav, Navbar} from "react-bootstrap";
 
 export const Navbarr = () => {
-  return<Navbar  expand="lg">
-      <Container>
-          <Navbar.Brand href="#home">
-             <img src={""} alt={"logo"}/>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" >
-      <span className={"navbar-toggler-icon"}> </span>
-          </Navbar.Toggle>
-          <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#skills">Skills</Nav.Link>
-                  <Nav.Link href="#projects">Projects</Nav.Link>
-              </Nav>
-              <span className={"navbar-text"}>
+    return <Navbar expand="lg">
+        <Container>
+            <Navbar.Brand href="#home">
+                <img src={"logo"} alt={"logo"} width={100}/>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav">
+                <span className={"navbar-toggler-icon"}>toogle </span>
+            </Navbar.Toggle>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#skills">Skills</Nav.Link>
+                    <Nav.Link href="#projects">Projects</Nav.Link>
+                </Nav>
+                <span className={"navbar-text"}>
                   <div className={"social-icon"}>
                       <a href={"#/"}><img src={""} alt={"facebook"}/></a>
                       <a href={"#/"}><img src={""} alt={"linkedin"}/></a>
                       <a href={"#/"}><img src={""} alt={"github"}/></a>
                   </div>
-
+                    <button className={"vvd"}
+                            onClick={() => console.log("connect")}><span> Let's Connect</span></button>
               </span>
-          </Navbar.Collapse>
-      </Container>
-  </Navbar>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
 }
