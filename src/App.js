c902a387320a7328935d9f1335b488d0796da19c
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-03-25 17:46:57 +0530

    Updated portfolio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
 import './App.css';
-import {Navbarr} from "./components/Navbarr";
+import {HomeNavbar} from "./components/HomeNavbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Banner} from "./components/Banner";
 
 function App() {
-  return <div>
-<Navbarr/>
-  </div>
+    return <div className={"App"}>
+        <HomeNavbar/>
+        <Banner/>
+    </div>
 }
 
 export default App;
