8f5ea149eb31fa3a4c2f619c823585ec5736bc38
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-06-27 15:24:46 +0530

    Updated portfolio

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,15 +1,21 @@
 import {BsGithub, BsInstagram, BsLinkedin} from "react-icons/bs";
 
 export default function SocialMedia() {
-    return<div className={"app__social"}>
+    return <div className={"app__social"}>
         <div>
-            <BsLinkedin/>
+            <a href="https://www.linkedin.com/in/gurjant-singh3" target="_blank" rel="noopener noreferrer">
+                <BsLinkedin/>
+            </a>
         </div>
         <div>
-            <BsGithub/>
+            <a href="https://github.com/Gurjantrandahwa" target="_blank" rel="noopener noreferrer">
+                <BsGithub/>
+            </a>
         </div>
         <div>
-            <BsInstagram/>
+            <a href="https://www.instagram.com/gur_jant_singh_r/" target="_blank" rel="noopener noreferrer">
+                <BsInstagram/>
+            </a>
         </div>
     </div>
 }
